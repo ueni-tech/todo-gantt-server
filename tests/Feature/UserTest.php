8e4a786b292597597b1d2d31b98535010b447371
ueni-tech <ueni.tech@gmail.com>
2024-03-17 20:23:16 +0900

    #3 test: test_should_list_empty_users

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -11,5 +12,18 @@ class UserTest extends TestCase
       $response = $this->get('/api/users');
   
       $response->assertStatus(200);
+    }
+
+    public function test_should_create_user(): void
+    {
+      $user = User::factory()->create();
+
+      $response = $this->post('/api/users');
+
+      $response->assertStatus(200)->assertJsonFragment([
+        'name' => $user->name,
+        'email' => $user->email,
+        'password' => $user->password,
+      ]);
     }
 }
