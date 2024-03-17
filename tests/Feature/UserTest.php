2de1d792c718cfbbe2e4278e389c96854d52fefe
ueni-tech <ueni.tech@gmail.com>
2024-03-17 15:25:23 +0900

    #3 test: Userテストの作成途中

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -8,7 +8,7 @@ class UserTest extends TestCase
 {
     public function test_should_list_empty_users(): void
     {
-      $response = $this->get('/');
+      $response = $this->get('/api/users');
   
       $response->assertStatus(200);
     }
